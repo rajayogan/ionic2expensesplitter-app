14812d1ab710adf835b11503e68597326cc875ae
rajayogan <rajayogan27@gmail.com>
2016-03-28 15:25:52 +0530

    Initial commit

---
+++ server.js
@@ -0,0 +1,19 @@
+var express = require('express'),
+app = express();
+var cors = require('cors');
+
+app.use(express.static('www'));
+app.use(cors());
+// CORS (Cross-Origin Resource Sharing) headers to support Cross-site HTTP requests
+// app.all('*', function(req, res, next) {
+//     res.header("Access-Control-Allow-Origin", "*");
+//     res.header("Access-Control-Allow-Headers", "X-Requested-With");
+//     next();
+// });
+
+
+app.set('port', process.env.PORT || 5000);
+
+app.listen(app.get('port'), function () {
+    console.log('Express server listening on port ' + app.get('port'));
+});
